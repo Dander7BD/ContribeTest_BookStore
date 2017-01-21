d080b87ba95b08d0b22b6d475dc9b88cbd2f8d60
Dander7BD <dander.1981@gmail.com>
2017-01-21 17:01:23 +0100

    AjaxBookSearch user control added
    Need JavaScript(JQuery) implementation

---
+++ BookStore/BookStore/AjaxBookSearch.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AjaxBookSearch.ascx.cs" Inherits="BookStore.Web.AjaxBookSearch" %>
+
+<div id="<%= this.ID %>" class="book-search">
+    <input type="search" class="filter-input" />
+    <ul class="response"/>
+</div>
---
+++ BookStore/BookStore/AjaxBookSearch.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookStore.Web
+{
+    public partial class AjaxBookSearch : System.Web.UI.UserControl
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender( e );
+
+            var scripts = ScriptManager.GetCurrent(this.Page).Scripts;
+            scripts.Add( new ScriptReference( "~/Scripts/jquery-3.1.1.min.js" ) );
+            scripts.Add( new ScriptReference( "~/Scripts/booksearch-ajax.js" ) );
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
